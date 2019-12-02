233fbf8d6ef7825600881257bb0a049d79749d49
Clyde Griffin <deezy13@gmail.com>
2019-12-02 17:07:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,9 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
+  
   def initialize(person_attributes)
-    person_attributes[:name]
+    @name = person_attributes[:name]
     
     
   end 
