ef619b4a90d75b23579f1d36827a2f5b7e595010
Clyde Griffin <deezy13@gmail.com>
2019-12-02 17:14:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,12 +8,18 @@ class Person
     @name = person_attributes[:name]
       @birthday = person_attributes[:birthday]
         @hair_color = person_attributes[:hair_color]
-          @name = person_attributes[:name]
-            @name = person_attributes[:name]
-              @name = person_attributes[:name]
-                @name = person_attributes[:name]
-     
-    
-    
-  end 
+          @eye_color = person_attributes[:eye_color]
+            @height = person_attributes[:height]
+              @weight = person_attributes[:weight]
+                @handed = person_attributes[:handed]
+                
+                @complexion = person_attributes[:complexion]
+                @t_shirt_size = person_attributes[:t_shirt_size]
+                @wrist_size = person_attributes[:wrist_size]
+                @glove_size = person_attributes[:glove_size]
+                @pant_length = person_attributes[:pant_length]
+                @pant_width = person_attributes[:pant_width]
+                
+                
+               end 
 end
